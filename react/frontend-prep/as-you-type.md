07bfec131382cc1c5b29f14f77eb9f90b874120d
Tony Nguyen <nguyen.tony1015@gmail.com>
2021-01-19 16:51:46 -0500

    adding explanation

---
+++ react/frontend-prep/as-you-type.md
@@ -0,0 +1,12 @@
+# Fetch Explanation 
+With the script that we have inside of this directory, what are we doing?
+
+1. We made a fetch call which returns a promise and we wrapped this fetch call inside of a function and the keyword `async`. The fetch call will return a promise object and we will await it or we could use .then(). You will need to .json() the returned response object.
+
+2. We made another function that will render the joke, this function will work hand in hand with the fn explained above, so what will that do? 
+
+We are creating an html element and selecting the element in which, where we want to append the child
+
+**Note** You could have this function be async and call the above function above, but we want to modularize it so we will do step 3.
+
+3. Finally we are making another function, this one doesn't have to be async, this function will be for the button on our html page that will select the button and it will have an eventlistener. The callback fn passed as the second argument will be async because this function will make the getJoke() call which means you need to await getJoke(). After that we are going to renderJoke()
--- react/frontend-prep/scripts/app-async.js
+++ react/frontend-prep/scripts/app-async.js
@@ -1,21 +1,46 @@
 // how to use async/await with fetch()
-async function main() {
+async function getJoke() {
     const response = await fetch("https://icanhazdadjoke.com", {
         headers: { Accept: "application/json" },
     });
     const jokeData = await response.json();
     console.log(jokeData)
     console.log(jokeData.joke)
+    return jokeData.joke
 }
-main()
 
 // another way you may see it
 // async function main() {
 //     const jokePromise = fetch("https://icanhazdadjoke.com", {
-//         headers: {Accept: "application/json"}
+//         headers: { Accept: "application/json" }
 //     })
 //     const response = await jokePromise
 //     const jokeData = await response.json()
 //     console.log(jokeData)
 //     console.log(jokeData.joke)
 // }
+
+function renderJoke(joke) {
+    // you will have to use await here because we are awaiting for the getJoke()
+    // const joke = await getJoke()
+
+    // create dom element 
+    const p = document.createElement('p')
+    p.textContent = joke;
+
+    // attach it to the existing dom element
+    const jokeBox = document.querySelector('.joke-box')
+    jokeBox.appendChild(p)
+}
+
+function main() {
+    const btn = document.querySelector("#get-joke")
+    btn.addEventListener('click', async (event) => {
+        // since this is a callback fn, you are able to pass event as an argument which will be the element
+        event.preventDefault();
+        const joke = await getJoke()
+        renderJoke(joke)
+    })
+}
+
+main()
